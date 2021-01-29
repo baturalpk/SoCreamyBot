56ee285d0d33619ff34560b45de644c28c6276cf
Baturalp KIZILTAN <60671090+baturalp-kiziltan@users.noreply.github.com>
2021-01-29 07:44:41 +0300

    Add comments

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 require('dotenv').config();
-const PREFIX = '#';
+const PREFIX = '#'; // The command prefix.
 
 const client = new Discord.Client({
     ws: {
@@ -12,26 +12,30 @@ client.on('ready', () => {
 
     client.user.setUsername('SoCreamyBot');
 
-    /*** Change game/app activity e.g. "Playing PacMan" ***/
+    /*
+    * Change game/app activity e.g. "PLAYING PacMan" or "STREAMING Mario "
+    * [ First element => type, Second element => Activity_String ]
+    */
     client.user.setActivity(process.env.ACTIVITY_STRING2, {
         type: "PLAYING"
     });
 });
 
 client.on('message', message => {
+    // Interact w/ the messages that starts with '#' prefix only.
     if (message.content.charAt(0) === PREFIX) {
-        if (message.content.startsWith('#hey'))
-            message.channel.send('Hello there ^^');
+        if (message.content.startsWith('#hey')) // #command1
+            message.channel.send('Hello there ^^'); // #answer1
 
-        else if (message.content.startsWith('#balan'))
-            message.channel.send(process.env.BALAN);
+        else if (message.content.startsWith('#balan')) // #command2
+            message.channel.send(process.env.BALAN); // #answer2
 
-        else if (message.content.startsWith('#cem'))
-            message.channel.send(process.env.CEM);
+        else if (message.content.startsWith('#cem')) // #command3
+            message.channel.send(process.env.CEM); // #answer3
 
         else
-            message.channel.send(process.env.NOT_FOUND);
+            message.channel.send(process.env.NOT_FOUND); // `desired command is not supported`
     }
 });
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN); // ACCESS_TOKEN (Define your own dotenv variable or directly pass into here.)
