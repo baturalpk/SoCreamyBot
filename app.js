85bdbe47e8439eb378514812a6cb698ace22e361
baturalp-kiziltan <baturalp.kiziltan@outlook.com>
2021-02-09 05:43:06 +0300

    Add more documentation & Make minor improvements

--- app.js
+++ app.js
@@ -1,51 +1,58 @@
-
 //** Dependencies, env variables ...
 require('dotenv').config();
 const Discord = require('discord.js');
 const ytdl = require("discord-ytdl-core");
 const { YTSearcher } = require('ytsearcher');
-const searcher = new YTSearcher(process.env.YT_API);
-const PREFIX = '#'; // The command prefix.
+
+// You should define your own YT_API token below (@process.env.YT_API) to avoid from restrictions of new YouTube Data API.
+const searcher = new YTSearcher(process.env.YT_API); 
+
+const PREFIX = '##'; // The default prefix for your all commands.
 
 //** Client configurations
 const client = new Discord.Client({
     ws: {
-        properties: { $browser: "Discord Android" } // Set status as 'mobile'
+        properties: { $browser: "Discord Android" } // Set status as 'mobile'.
     }
 });
 
 client.on('ready', () => {
     console.log(`The ${client.user.username} is up!`);
 
-    client.user.setUsername('SoCreamyBot');
+    client.user.setUsername('SoCreamyBot'); // Change bot's name if you desire.
 
-    /*
-    * Change game/app activity e.g. "PLAYING PacMan" or "STREAMING Mario "
-    * [ First element => type, Second element => Activity_String ]
+    
+   /* Change game/app activity by replacing "process.env.ACTIVITY_STRING" to a custom string value or defining your own dotenv variable.
+    * Type command does not accept value other than the following => 'PLAYING' | 'STREAMING' | 'LISTENING' | 'WATCHING' | 'CUSTOM_STATUS' | 'COMPETING'
+    * It shows like "PLAYING PacMan" or "STREAMING Mario  [First element => type, Second element => Activity_String]
+    * Look at Discord.js documentation for more information.
     */
-    client.user.setActivity(process.env.ACTIVITY_STRING2, {
+    client.user.setActivity(process.env.ACTIVITY_STRING, {
         type: "PLAYING"
     });
 });
 
 //** User-command handlers
 client.on('message', async message => {
-    // Interact w/ the messages that starts with '#' prefix only.
-    if (message.content.charAt(0) === PREFIX) {
-        if (message.content.startsWith('#hey')) // #command1
+    // Interact w/ the messages that starts with defined PREFIX only.
+    if (message.content.startsWith(PREFIX)) {
+        if (message.content.startsWith(PREFIX + 'hey')) // #command1
             message.channel.send('Hello there ^^'); // #answer1
 
-        else if (message.content.startsWith('#balan')) // #command2
+        else if (message.content.startsWith(PREFIX + 'balan')) // #command2
             message.channel.send(process.env.BALAN); // #answer2
 
-        else if (message.content.startsWith('#cem')) // #command3
+        else if (message.content.startsWith(PREFIX + 'cem')) // #command3
             message.channel.send(process.env.CEM); // #answer3
 
-        else if (message.content.startsWith('#calc'))
-            message.channel.send(DoSomeArithmetic(message.content));
+        else if (message.content.startsWith(PREFIX + 'calc')) // #command4
+            message.channel.send(DoSomeArithmetic(message.content)); // #answer4 ...
 
-        else if (message.content.startsWith('#play'))
+        else if (message.content.startsWith(PREFIX + 'play'))
             PlayMusic(message);
+
+        else if (message.content.startsWith(PREFIX + 'clear'))
+            ClearChat(message);
 
         else
             message.channel.send(process.env.NOT_FOUND); // `desired command is not supported`
@@ -53,11 +60,11 @@ client.on('message', async message => {
 });
 
 //** Start the bot
-client.login(process.env.TOKEN); // ACCESS_TOKEN (Define your own dotenv variable or directly pass into here.)
+client.login(process.env.TOKEN); // DISCORD_ACCESS_TOKEN (Define your own token as dotenv variable or directly pass into here.)
 
 /**
- * @description SIMPLE MATH SOLVER
- * @param {Discord.message} msg
+ * Solves four basic math operations (addition, subtraction, multiplication, division)
+ * @param {Discord.Message} msg
  * @returns {string} 'result' of any arithmetic operation between 2 numbers OR 'error message'
  */
 function DoSomeArithmetic(msg) {
@@ -106,29 +113,35 @@ function DoSomeArithmetic(msg) {
 }
 
 /**
- * @param {Discord.message} message 
- * @returns
+ * Plays your favorite music from YouTube.
+ * @param {Discord.Message} message
  */
 async function PlayMusic(message) {
     if (! message.member.voice.channel) 
         return message.channel.send("Please join a voice channel...");
 
-    let result = await searcher.search(message.content.substr(6));
-    let URL = result.currentPage[0].url;
-    console.log(URL);
+    let searchValue = message.content.substr(5 + PREFIX.length)
 
-    let stream = ytdl(URL, {
-        filter: "audioonly",
-        opusEncoded: false,
-        fmt: "mp3",
-        encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
-    });
+    if (searchValue.length == 0) {
+        message.channel.send("Invalid search parameters. Try again with a proper song name!")
+        return;
+    }
+
+    let result = await searcher.search(searchValue);
+    let URL = result.currentPage[0].url;
+    message.channel.send(`Playing => ${URL}`);
 
     message.member.voice.channel.join().then(connection => {
-        let dispatcher = connection.play(stream, {
+        let dispatcher = connection.play(
+            ytdl(URL, {
+                filter: "audioonly",
+                opusEncoded: false,
+                fmt: "mp3",
+                encoderArgs: ['-af', 'dynaudnorm=f=100']}), {
             type: "unknown"
         })
         .on("finish", () => {
+            message.channel.send(`I'm leaving bye 👋`);
             message.guild.me.voice.channel.leave()
         });
 
